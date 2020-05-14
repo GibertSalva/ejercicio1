a70dd52441f9ad88279e66b5c31a73058a103d76
Salvador Gibert <salvigibert@gmail.com>
2020-05-14 20:28:42 -0300

    creamos clases autor y libro

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Autor (models.Model):
+    nombre = models.Charfield(max_length = 20)
+
+class Libro(models.Model):
+    titulo = models.Charfield(max_length = 20)
+    editorial = models.Charfield(max_length = 20)
+    paginas = models.Charfield(max_length = 20)
+    autor = models.ForeignKey("Autor", on_delete=models.CASCADE,null = False,)
