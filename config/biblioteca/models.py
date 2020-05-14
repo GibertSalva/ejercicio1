b319007faf80f4a32ee61bfb7d913a2a02e13f69
Salvador Gibert <salvigibert@gmail.com>
2020-05-14 20:33:55 -0300

    Creacion de clase Ejemplar y correccion de errores

--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Autor (models.Model):
-    nombre = models.Charfield(max_length = 20)
+    nombre = models.CharField(max_length = 20)
 
 class Libro(models.Model):
-    titulo = models.Charfield(max_length = 20)
-    editorial = models.Charfield(max_length = 20)
-    paginas = models.Charfield(max_length = 20)
+    titulo = models.CharField(max_length = 20)
+    editorial = models.CharField(max_length = 20)
+    paginas = models.CharField(max_length = 20)
     autor = models.ForeignKey("Autor", on_delete=models.CASCADE,null = False,)
+
+class Ejemplar(models.Model):
+    localizacion = models.CharField(max_length = 20)
+    libro = models.ForeignKey("Libro", on_delete=models.CASCADE,null = False,)
